fae3a1ac90b0316f65b9b6171b26e55882b69b0d
Shinmera <shinmera@tymoon.eu>
2017-05-28 15:30:55 +0200

    Lots of hypothetical stuff.

--- source.lisp
+++ source.lisp
@@ -6,5 +6,88 @@
 
 (in-package #:org.shirakumo.fraf.harmony)
 
-(defclass source ()
-  ((looping-p :initarg :loop :initform NIL :accessor looping-p)))
+(defclass source (cl-mixed:source)
+  ((looping-p :initarg :loop :initform NIL :accessor looping-p)
+   (paused-p :initarg :paused-p :initform NIL :accessor paused-p)
+   (ended-p :initform NIL :accessor ended-p)
+   (decoder :initform (lambda (samples source)) :accessor decoder)
+   (server :initarg :server :initform NIL :accessor server)
+   (sample-position :initform 0 :accessor sample-position)
+   (remix-factor :initform 0 :accessor remix-factor)))
+
+(defmethod initialize-instance ((source source) &rest args &key server)
+  (unless server
+    (error "The SERVER initarg is required, but not given."))
+  (setf (server source) server)
+  (apply #'call-next-method
+         :channel (initialize-channel source)
+         :samplerate (samplerate server)
+         args))
+
+(defmethod initialize-instance :after ((source source) &key)
+  (setf (remix-factor source) (/ (samplerate (server source))
+                                 (cl-mixed:samplerate (cl-mixed:channel source)))))
+
+(defgeneric initialize-channel (source))
+
+(defmethod (setf paused-p) :before (value (source source))
+  (when value
+    (unless (paused-p source)
+      (with-server-lock ((server source))
+        (mapc #'cl-mixed:clear (cl-mixed:outputs source))))))
+
+(defmethod pause ((source source))
+  (setf (paused-p source) T))
+
+(defmethod resume ((source source))
+  (when (ended-p source)
+    (seek source 0))
+  (setf (paused-p source) NIL))
+
+(defmethod stop ((source source))
+  (setf (ended-p source) T))
+
+(defgeneric seek (source position &key mode by))
+
+(defmethod seek :around ((source source) position &key (mode :absolute) (by :sample))
+  (ecase by
+    (:second
+     (setf position (round (* position (cl-mixed:samplerate (cl-mixed:channel source)))))))
+  (ecase mode
+    (:relative
+     (setf mode :absolute)
+     (incf position (sample-position source))))
+  (call-next-method source position :mode :absolute :by :sample)
+  (setf (ended-p source) NIL)
+  (setf (sample-position source) position))
+
+(defmethod position ((source source))
+  (values (sample-position source)
+          (coerce (/ (sample-position source)
+                     (cl-mixed:samplerate (cl-mixed:channel source)))
+                  'single-float)))
+
+(defmethod (setf position) (value (source source))
+  (seek source value :mode :absolute :by :sample))
+
+(cffi:defcallback source-mix :void ((samples cl-mixed-cffi:size_t) (segment :pointer))
+  (let* ((source (cl-mixed:pointer->object segment))
+         (real-samples (* samples (remix-factor source))))
+    (unless (paused-p source)
+      ;; We need to handle ended-p like this in order to make
+      ;; sure that the last samples that were processed before
+      ;; ended-p was set still get out before we clear the
+      ;; buffers (by setting paused-p to T).
+      (cond ((ended-p source)
+             (setf (paused-p source) T))
+            (T
+             ;; Decode samples from the source
+             (funcall (decoder source) real-samples source)
+             ;; Process the channel to get the samples into buffers
+             (cffi:foreign-funcall-pointer
+              (chan-function source) ()
+              cl-mixed-cffi:size_t samples
+              :pointer segment
+              :void)
+             ;; Count current stream position
+             (incf (sample-position source) real-samples))))))
--- sources/mp3.lisp
+++ sources/mp3.lisp
@@ -9,47 +9,45 @@
   (:nicknames #:harmony-mp3)
   (:use #:cl #:harmony)
   (:export
-   ))
+   #:mp3-source))
 
-(defclass mp3-source (cl-mixed:virtual source)
+(defclass mp3-source (source)
   ((file :initform NIL :accessor file)
-   (source :initform NIL :accessor source)
    (samplesize :initform NIL :accessor samplesize)))
 
-(defmethod initialize-instance :after ((mp3 mp3-source) &key file)
-  (let ((file (cl-mpg123:make-file file :accepted-format '(44100 2 :float))))
+(defmethod initialize-channel ((source mp3-source))
+  (let ((file (cl-mpg123:make-file file :accepted-format (list (samplerate (server mp3))
+                                                               2 :float))))
     (cl-mpg123:connect file)
-    (setf (cl-mixed-cffi:direct-segment-mix (cl-mixed:handle mp3)) (cffi:callback mix))))
+    (multiple-value-bind (rate channels encoding) (cl-mpg123:file-format file)
+      (cl-mixed:make-channel NIL
+                             (* (buffersize (server source))
+                                (cl-mixed:samplesize encoding))
+                             encoding
+                             channels
+                             :alternating
+                             rate))))
 
-(defmethod (setf cl-mixed:output-field) ((value buffer) (field (eql :buffer)) location (mp3 mp3-source))
-  (cl-mixed-cffi:segment-set-out field location (cl-mixed:handle value) (source mp3)))
+(defmethod seek ((source mp3-source) position &key mode by)
+  (cl-mpg123:seek (file source) position :mode mode :by by))
 
-(defmethod cl-mixed:info ((mp3 mp3-source))
-  (list :name "mp3-source"
-        :description "Playback from an MP3 file."
-        :flags ()
-        :min-inputs 0
-        :max-inputs 0
-        :outputs (cl-mpg123:channels (file mp3))
-        :fields ()))
-
-(cffi:defcallback mix :void ((samples cl-mixed-cffi:size_t) (segment :pointer))
-  (let* ((mp3 (cl-mixed:pointer->object segment))
-         (file (file mp3))
-         (source (source mp3))
-         (buffer (cl-mixed-cffi:channel-data source)))
-    (let* ((samples (* samples (samplesize mp3)))
-           (read (cl-mpg123:read-directly file buffer samples)))
-      (when (< read samples)
-        (cond ((looping-p mp3)
-               ;; We loop to catch corner cases where the file does not even fill a single
-               ;; buffer. This is exceedingly unlikely to ever be the case, but w/e.
-               (loop while (< read samples)
-                     do (cl-mpg123:seek file 0)
-                        (incf read (cl-mpg123:read-directly file buffer (- samples read)))))
-              (T
-               ;; Make sure to pad out the rest of the buffer with zeroes.
-               (loop for i from read below samples
-                     do (setf (cffi:mem-aref buffer :uint8) 0))
-               (stop mp3)))))
-    (cl-mixed-cffi:segment-mix samples source)))
+(defun decode (samples source)
+  (let* ((file (file source))
+         (channel (cl-mixed:channel source))
+         (buffer (cl-mixed-cffi:channel-data channel))
+         (bytes (* samples (cl-mixed:samplesize (cl-mixed-cffi:channel-encoding channel))))
+         (read (cl-mpg123:read-directly file buffer bytes)))
+    (when (< read bytes)
+      (cond ((looping-p source)
+             ;; We loop to catch corner cases where the file does not even fill a single
+             ;; buffer. This is exceedingly unlikely to ever be the case, but w/e.
+             (loop while (< read bytes)
+                   do (cl-mpg123:seek file 0)
+                      (let ((new-read (cl-mpg123:read-directly file buffer (- bytes read))))
+                        (incf read new-read)
+                        (setf (sample-position source) new-read))))
+            (T
+             ;; Make sure to pad out the rest of the buffer with zeroes.
+             (loop for i from read below bytes
+                   do (setf (cffi:mem-aref buffer :uint8) 0))
+             (setf (ended-p source) T))))))
