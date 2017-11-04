54c2d80d444476d6377edc60ce14cfa36ca45fe3
Shinmera <shinmera@tymoon.eu>
2017-11-04 12:29:53 +0100

    Some more progress on this shit.

--- drains/wasapi-cffi.lisp
+++ drains/wasapi-cffi.lisp
@@ -8,8 +8,88 @@
 (defpackage #:harmony-wasapi-cffi
   (:nicknames #:org.shirakumo.fraf.harmony.drains.wasapi.cffi)
   (:use #:cl #:cffi)
+  (:shadow #:byte)
   (:export
-   ))
+   #:ole32
+   #:audclnt-sharemode
+   #:audclnt-bufferflags
+   #:hresult
+   #:dataflow
+   #:role
+   #:guid
+   #:guid-data1
+   #:guid-data2
+   #:guid-data3
+   #:guid-data4
+   #:waveformat-ex
+   #:waveformat-ex-format-tag
+   #:waveformat-ex-channels
+   #:waveformat-ex-samples-per-sec
+   #:waveformat-ex-avg-bytes-per-sec
+   #:waveformat-ex-block-align
+   #:waveformat-ex-bits-per-sample
+   #:waveformat-ex-size
+   #:waveformat-extensible
+   #:waveformat-extensible-format
+   #:waveformat-extensible-samples
+   #:waveformat-extensible-dw-channel-mask
+   #:waveformat-extensible-sub-format
+   #:com
+   #:vtbl
+   #:imm-device-enumerator
+   #:imm-device-enumerator-query-interface
+   #:imm-device-enumerator-add-ref
+   #:imm-device-enumerator-release
+   #:imm-device-enumerator-enum-audio-endpoints
+   #:imm-device-enumerator-get-default-audio-endpoint
+   #:imm-device-enumerator-get-device
+   #:imm-device-enumerator-register-endpoint-notification-callback
+   #:imm-device-enumerator-unregister-endpoint-notification-callback
+   #:imm-device-collection
+   #:imm-device-collection-query-interface
+   #:imm-device-collection-add-ref
+   #:imm-device-collection-release
+   #:imm-device-collection-get-count
+   #:imm-device-collection-item
+   #:imm-device
+   #:imm-device-query-interface
+   #:imm-device-add-ref
+   #:imm-device-release
+   #:imm-device-activate
+   #:imm-device-open-property-store
+   #:imm-device-get-id
+   #:imm-device-get-state
+   #:i-audio-client
+   #:i-audio-client-query-interface
+   #:i-audio-client-add-ref
+   #:i-audio-client-release
+   #:i-audio-client-initialize
+   #:i-audio-client-get-buffer-size
+   #:i-audio-client-get-stream-latency
+   #:i-audio-client-get-current-padding
+   #:i-audio-client-is-format-supported
+   #:i-audio-client-get-mix-format
+   #:i-audio-client-get-device-period
+   #:i-audio-client-start
+   #:i-audio-client-stop
+   #:i-audio-client-reset
+   #:i-audio-client-set-event-handle
+   #:i-audio-client-get-service
+   #:i-audio-render-client
+   #:i-audio-render-client-query-interface
+   #:i-audio-render-client-add-ref
+   #:i-audio-render-client-release
+   #:i-audio-render-client-get-buffer
+   #:i-audio-render-client-release-buffer
+   #:co-initialize
+   #:co-uninitialize
+   #:co-create-instance
+   #:av-set-mm-thread-characteristics
+   #:av-revert-mm-thread-characteristics
+   #:wait-for-single-object
+   #:create-event
+   #:close-handle
+   #:set-event))
 (in-package #:org.shirakumo.fraf.harmony.drains.wasapi.cffi)
 
 (define-foreign-library ole32
@@ -28,7 +108,8 @@
            (vtbl ,structg))
           ,options
           :pointer ,structg
-          ,@(mapcar (lambda (a) (reverse a)) args)
+          ,@(loop for (name type) in args
+                  collect type collect name)
           ,return-type)))))
 
 (defmacro defcomstruct (name &body methods)
@@ -50,6 +131,8 @@
 ;; https://github.com/EddieRingle/portaudio/blob/master/src/hostapi/wasapi/mingw-include/audioclient.h
 ;; https://github.com/EddieRingle/portaudio/blob/master/src/hostapi/wasapi/mingw-include/mmdeviceapi.h
 
+;; FIXME: turn into actual structs
+;; FIXME: better naming
 (define-constant IID_IAudioClient #(#x1CB9AD4C #xDBFA #x4c32 #xB1 #x78 #xC2 #xF5 #x68 #xA7 #x03 #xB2))
 (define-constant IID_IAudioRenderClient #(#xF294ACFC #x3146 #x4483 #xA7 #xBF #xAD #xDC #xA7 #xC2 #x60 #xE2))
 (define-constant CLSID_MMDeviceEnumerator #(#xBCDE0395 #xE52F #x467C #x8E #x3D #xC4 #x57 #x92 #x91 #x69 #x2E))
@@ -60,12 +143,18 @@
 (define-constant CLSCTX-ALL 23)
 (define-constant INFINITE -1)
 
-(defctype hresult :uint32)
 (defctype word :uint16)
 (defctype dword :uint32)
 (defctype refiid :pointer)
 (defctype refclsid :pointer)
 (defctype lpunknown :pointer)
+(defctype byte :uint8)
+(defctype ulong :unsigned-long)
+(defctype wstring :pointer)
+(defctype reference-time :long-long)
+(defctype lpcguid :pointer)
+(defctype handle :pointer)
+(defctype lpdword :pointer)
 
 (defcenum audclnt-sharemode
   :shared
@@ -76,7 +165,7 @@
   (:silent 2)
   (:timestamp-error 4))
 
-(defcenum result
+(defcenum hresult
   (:ok 0)
   (:false 1)
   (:already-initialized 2290679810)
@@ -96,13 +185,25 @@
   (:out-of-memory 2147942414)
   (:bad-pointer 2147500035))
 
-(defcstruct (guid)
+(defcenum dataflow
+  :render
+  :capture
+  :all
+  :dataflow-enum-count)
+
+(defcenum role
+  :console
+  :multimedia
+  :communications
+  :role-enum-count)
+
+(defcstruct (guid :conc-name guid-)
   (data1 dword)
   (data2 word)
   (data3 word)
   (data4 byte :count 8))
 
-(defcstruct (waveformat-ex)
+(defcstruct (waveformat-ex :conc-name waveformat-ex-)
   (format-tag word)
   (channels word)
   (samples-per-sec dword)
@@ -111,28 +212,28 @@
   (bits-per-sample word)
   (size word))
 
-(defcstruct (waveformat-extensible)
-  (format waveformat-ex)
+(defcstruct (waveformat-extensible :conc-name waveformat-extensible-)
+  (format (:struct waveformat-ex))
   (samples word)
   (dw-channel-mask word)
-  (sub-format guid))
+  (sub-format (:struct guid)))
 
 (defcstruct (com :conc-name ||)
   (vtbl :pointer))
 
 (defcomstruct imm-device-enumerator
   (enum-audio-endpoints hresult
-    (data-flow edataflow)
+    (data-flow dataflow)
     (state-mask dword)
     (devices :pointer))
 
   (get-default-audio-endpoint hresult
-    (data-flow edataflow)
-    (role erole)
+    (data-flow dataflow)
+    (role role)
     (endpoint :pointer))
 
   (get-device hresult
-    (pwstrid lpcwstr)
+    (pwstrid wstring)
     (device :pointer))
 
   (register-endpoint-notification-callback hresult
@@ -149,7 +250,7 @@
     (device-id :uint)
     (device :pointer)))
 
-(defcomstruct (imm-device %imm-device-)
+(defcomstruct imm-device
   (activate hresult
     (id refiid)
     (cls-ctx dword)
@@ -166,7 +267,7 @@
   (get-state hresult
     (state :pointer)))
 
-(defcomstruct (i-audio-client :conc-name %i-audio-client-)
+(defcomstruct i-audio-client
   (initialize hresult
     (share-mode audclnt-sharemode)
     (stream-flags dword)
@@ -231,7 +332,7 @@
   (ppv :pointer))
 
 (defcfun (av-set-mm-thread-characteristics "AvSetMmThreadCharacteristics") handle
-  (task-name lpctstr)
+  (task-name wstring)
   (task-index lpdword))
 
 (defcfun (av-revert-mm-thread-characteristics "AvRevertMmThreadCharacteristics") :bool
@@ -242,7 +343,7 @@
   (milliseconds dword))
 
 (defcfun (create-event "CreateEvent") handle
-  (event-attribute lpsecurity-attributes)
+  (event-attribute :pointer)
   (manual-reset :bool)
   (initial-state :bool)
   (name :string))
