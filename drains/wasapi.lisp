d6d72996647c8341728edb846ee63acc69d15a8a
Shinmera <shinmera@tymoon.eu>
2017-11-07 18:36:16 +0100

    Woww you are a fuckiinn idiot man

--- drains/wasapi.lisp
+++ drains/wasapi.lisp
@@ -109,11 +109,9 @@ Author: Nicolas Hafner <shinmera@tymoon.eu>
         (let ((closest (cffi:mem-ref closest :pointer)))
           (unwind-protect
                (multiple-value-bind (samplerate channels sample-format)
-                   (cond ((cffi:null-pointer-p closest)
-                          (values))
-                         ((eql :ok pass)
+                   (cond ((and (eql :ok pass))
                           (values samplerate channels sample-format))
-                         (T
+                         ((not (cffi:null-pointer-p closest))
                           (harmony-wasapi-cffi:decode-wave-format closest)))
                  (values (eql :ok pass) samplerate channels sample-format))
             (harmony-wasapi-cffi:co-task-mem-free closest)))))))
