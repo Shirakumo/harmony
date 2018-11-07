8c043b0a082980237c254e84c57e16b80d87c71a
terminal625 <zpersoncool@gmail.com>
2018-11-07 12:43:14 -0500

    fix arguments to cl:apply in harmony-simple:decode
    see: http://clhs.lisp.se/Body/26_glo_s.htm#spreadable_argument_list_designator
    and: http://clhs.lisp.se/Body/f_apply.htm

--- simple/default.lisp
+++ simple/default.lisp
@@ -58,7 +58,7 @@
 (defun decode (source-ish &rest initargs &key (server *server*) &allow-other-keys)
   (let ((initargs (copy-list initargs)))
     (remf initargs :server)
-    (apply #'harmony:decode source-ish initargs :samplerate (samplerate server))))
+    (apply #'harmony:decode source-ish :samplerate (samplerate server) initargs)))
 
 (defmethod add ((source source) (name symbol))
   (add source (ensure-segment name *server*)))
