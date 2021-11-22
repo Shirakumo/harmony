423afa3f2a94844e3c67e3ae8cde0b62b8c7c51c
Shinmera <shinmera@tymoon.eu>
2021-11-22 22:36:21 +0100

    Add support for mpt (tracker/module) files.

--- voice.lisp
+++ voice.lisp
@@ -75,7 +75,11 @@
         (:wav (maybe-make-drain org.shirakumo.fraf.mixed.wav cl-mixed-wav in-memory-source))
         (:flac (maybe-make-drain org.shirakumo.fraf.mixed.flac cl-mixed-flac))
         (:ogg (maybe-make-drain org.shirakumo.fraf.mixed.vorbis cl-mixed-vorbis))
-        (:oga (maybe-make-drain org.shirakumo.fraf.mixed.vorbis cl-mixed-vorbis))))))
+        (:oga (maybe-make-drain org.shirakumo.fraf.mixed.vorbis cl-mixed-vorbis))
+        ((:mptm :mod :s3m :xm :it :669 :amf :ams :c67 :dbm :digi :dmf :dsm :dtm :far
+          :imf :ice :j2b :m15 :mdl :med :mms :mt2 :mtm :nst :okt :plm :psm :pt36 :ptm
+          :sfx :sfx2 :st26 :stk :stm :stp :ult :wow :gdm :mo3 :oxm :umx :xpk :ppm :mmcmp)
+         (maybe-make-drain org.shirakumo.fraf.mixed.mpt cl-mixed-mpt))))))
 
 (defmethod initialize-instance :after ((voice voice) &rest args &key source effects channels (on-end :free) &allow-other-keys)
   (flet ((free (_) (declare (ignore _))
